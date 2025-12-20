2cac796349aa7de78d30b1d25296fd8f4a73cadb
Vansh <z5482641@ad.unsw.edu.au>
2025-12-20 22:36:13 +1100

    added key-value converstion of registry prints to stdout

--- registry_parser.py
+++ registry_parser.py
@@ -1,20 +1,40 @@
 import typer
 from Registry import Registry
 from typing_extensions import Annotated
+from typing import Dict
+import pdb
+import json
 
 app = typer.Typer(help="Parse Windows Registry files incluing NTUSER.DAT")
 
 
-def rec(key, depth=0):
-    print("\t" * depth + key.path())
-
+def rec(key: Registry.RegistryKey, out: Dict):
+    if len(key.values()) != 0:
+        for value in key.values():
+            if not isinstance(value.value(), bytes) :
+                out["value"] = {"name": value.name(), "type": value.value_type_str(), "value": value.value()}
+            else:
+                out["value"] = {"name": value.name(), "type": value.value_type_str(), "valueAsString": value.value().hex()}
+                
+    if len(key.subkeys()) == 0:
+        return out
     for subkey in key.subkeys():
-        rec(subkey, depth + 1)
+        out[subkey.name()] = {}
+        rec(subkey, out[subkey.name()])
+
 
 @app.command()
-def print_registry_file(registry_file_path: Annotated[str, typer.Argument(help="Path to registry file")]):
+def print_registry_file(
+    registry_file_path: Annotated[str, typer.Argument(help="Path to registry file")],
+    is_json: Annotated[bool, typer.Option("-j")] = False
+):
     reg = Registry.Registry(registry_file_path)
-    rec(reg.root())
+    out = {}
+    rec(reg.root(), out)
+    if is_json:
+        print(json.dumps(out))
+    else:
+        print(out)
 
 
 if __name__ == "__main__":

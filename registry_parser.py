33389414e3bfec6db8f903cf06cd382cf9b89495
Vansh <z5482641@ad.unsw.edu.au>
2026-01-05 20:10:01 +1100

    Fixed memory hog issue

--- registry_parser.py
+++ registry_parser.py
@@ -2,40 +2,67 @@ import typer
 from Registry import Registry
 from typing_extensions import Annotated
 from typing import Dict
-import pdb
 import json
+import sys
+import io
 
 app = typer.Typer(help="Parse Windows Registry files incluing NTUSER.DAT")
 
 
-def rec(key: Registry.RegistryKey, out: Dict):
-    if len(key.values()) != 0:
-        for value in key.values():
-            if not isinstance(value.value(), bytes) :
-                out["value"] = {"name": value.name(), "type": value.value_type_str(), "value": value.value()}
+def rec(key: Registry.RegistryKey, out: io.TextIOWrapper):
+
+    current_key_data = {
+        "path": key.path(),
+        "timestamp": key.timestamp().isoformat(),
+        "values": []
+    }
+
+    
+    for value in key.values():
+            data = value.value()
+            
+            item = {
+                "name": value.name(),
+                "type": value.value_type_str()
+            }
+
+            if isinstance(data, bytes):
+                item["valueAsString"] = data.hex()
             else:
-                out["value"] = {"name": value.name(), "type": value.value_type_str(), "valueAsString": value.value().hex()}
+                item["value"] = data
                 
-    if len(key.subkeys()) == 0:
-        return out
+            current_key_data["values"].append(item)
+                
+    out.write(json.dumps(current_key_data) + "\n")
+
     for subkey in key.subkeys():
-        out[subkey.name()] = {}
-        rec(subkey, out[subkey.name()])
+        rec(subkey, out)
+
+
 
 
 @app.command()
 def print_registry_file(
     registry_file_path: Annotated[str, typer.Argument(help="Path to registry file")],
-    is_json: Annotated[bool, typer.Option("-j")] = False
+    output_path: Annotated[str, typer.Option("-o")] = None,
+    csv_out: Annotated[bool, typer.Option("-c")] = False
 ):
     reg = Registry.Registry(registry_file_path)
-    out = {}
-    rec(reg.root(), out)
-    if is_json:
-        print(json.dumps(out))
+
+    if output_path is None:
+        output_path = sys.stdout
     else:
-        print(out)
+        output_path = open(output_path, 'w')
+    
+    rec(reg.root())
+
+
+    if output_path is not sys.stdout:
+        output_path.close()
+    
 
 
 if __name__ == "__main__":
     app()
+
+
